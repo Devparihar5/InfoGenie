37c0a9515d26c68e7b8448cbe6e4e4b27047b09a
dev-celebgaze <devendra@celebgaze.ai>
2024-05-02 17:26:11 +0530

    first realease...

---
+++ main.py
@@ -0,0 +1,111 @@
+from dotenv import load_dotenv
+import os
+import pickle
+import time
+import streamlit as st
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+
+# Load environment variables
+load_dotenv()
+
+# Configure Google Generative AI
+from google.generativeai import configure
+configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+class DataLoader:
+    def __init__(self, urls):
+        self.urls = urls
+        self.loader = UnstructuredURLLoader(urls=urls)
+
+    def load_data(self):
+        return self.loader.load()
+
+class TextSplitter:
+    def __init__(self, separators, chunk_size, chunk_overlap):
+        self.separators = separators
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+        self.text_splitter = RecursiveCharacterTextSplitter(
+            separators=separators,
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap
+        )
+
+    def split_documents(self, data):
+        return self.text_splitter.split_documents(data)
+
+class EmbeddingsGenerator:
+    def __init__(self, model_path):
+        self.embeddings = GoogleGenerativeAIEmbeddings(model=model_path)
+
+    def generate_embeddings(self, documents):
+        vectorstore = FAISS.from_documents(documents=documents, embedding=self.embeddings)
+        return vectorstore
+
+class StreamlitApp:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.query = None
+        self.vectorIndex = None
+
+    def run(self):
+        st.title("Research Assistant! 🤖")
+        st.subheader("Type your query and let's uncover some answers! 🕵️‍♂️")
+
+        st.sidebar.title("Add URLs on topics you'd like to explore and see the magic unfold! ✨")
+        urls = [st.sidebar.text_input(f"URL{i+1}") for i in range(4)]
+        process = st.sidebar.button("Run 🚀")
+
+        if process:
+            data_loader = DataLoader(urls)
+            data = data_loader.load_data()
+
+            text_splitter = TextSplitter(separators=["\n\n", "\n", " "], chunk_size=1000, chunk_overlap=100)
+            docs = text_splitter.split_documents(data)
+
+            embeddings_generator = EmbeddingsGenerator(model_path="models/embedding-001")
+            vectorstore = embeddings_generator.generate_embeddings(docs)
+
+            with open(self.file_path, "wb") as f:
+                pickle.dump(vectorstore, f)
+
+        self.query = st.text_input(" ")
+        if self.query:
+            if os.path.exists(self.file_path):
+                with open(self.file_path, "rb") as f:
+                    self.vectorIndex = pickle.load(f)
+                    self.process_query()
+
+    def process_query(self):
+        llm_prompt_template = """You are an assistant for question-answering tasks.
+        Use the following context to answer the question.
+        If you don't know the answer, just say that you don't know.
+        Use five sentences maximum and keep the answer concise.\n
+        Question: {question} \nContext: {context} \nAnswer:"""
+        retriever = self.vectorIndex.as_retriever(search_kwargs={"k": 6})
+
+        llm_prompt = PromptTemplate.from_template(llm_prompt_template)
+
+        # Chain for processing query
+        rag_chain = (
+            {"context": retriever | (lambda docs: "\n\n".join(doc.page_content for doc in docs)),
+             "question": RunnablePassthrough()}
+            | llm_prompt
+            | ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.7, top_p=0.85)
+            | StrOutputParser()
+        )
+        result = rag_chain.invoke(self.query)
+
+        # Display result
+        st.subheader("Answer 💡")
+        st.write(result)
+
+if __name__ == "__main__":
+    app = StreamlitApp(file_path="faiss_store_gemini.pkl")
+    app.run()
